e23d4f68c43bc717956c08c89609a7bd893bf9b6
Samuel <sam.law.sam@gmail.com>
2018-09-04 22:50:24 +0800

    Update Sheet Service

--- src/main/java/com/arvato/jesy/lifematters/services/SheetService.java
+++ src/main/java/com/arvato/jesy/lifematters/services/SheetService.java
@@ -1,13 +1,14 @@
 package com.arvato.jesy.lifematters.services;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 
 import com.arvato.jesy.lifematters.entities.Personnel;
 import com.arvato.jesy.lifematters.repositories.PersonnelRepository;
 import com.google.api.services.sheets.v4.Sheets;
-import com.google.api.services.sheets.v4.model.Spreadsheet;
-import com.google.api.services.sheets.v4.model.SpreadsheetProperties;
+import com.google.api.services.sheets.v4.model.UpdateValuesResponse;
+import com.google.api.services.sheets.v4.model.ValueRange;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -27,9 +28,21 @@ public class SheetService{
         List<Personnel> personnels = personnelRepository.findAll();
 
 
-        Spreadsheet spreadSheet = new Spreadsheet().setProperties(new SpreadsheetProperties().setTitle("My Spreadsheet"));
-        Spreadsheet result = sheetsService.spreadsheets().create(spreadSheet).execute();
+//        Spreadsheet spreadSheet = new Spreadsheet().setProperties(new SpreadsheetProperties().setTitle("My Spreadsheet"));
+  //      Spreadsheet result = sheetsService.spreadsheets().create(spreadSheet).execute();
 
+        ValueRange body = new ValueRange()
+        .setValues(Arrays.asList(
+        Arrays.asList("Expenses January"), 
+        Arrays.asList("books", "30"), 
+        Arrays.asList("pens", "10"),
+        Arrays.asList("Expenses February"), 
+        Arrays.asList("clothes", "20"),
+        Arrays.asList("shoes", "5")));
+        UpdateValuesResponse result = sheetsService.spreadsheets().values()
+        .update(SHEET_ID, "A1", body)
+        .setValueInputOption("RAW")
+        .execute();
        // assertThat(result.getSpreadsheetId()).isNotNull();
     }
 }
